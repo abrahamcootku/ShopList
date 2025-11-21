2be208b0675599b2551d8754d386e179ae9e878b
Abraham Samuel Coot kU <23470124@campeche.tecnm.mx>
2025-11-21 10:39:16 -0600

    utiliza el patron MVVM sin bibliotecas

--- ShopList.Gui/ViewModels/ShopListViewModel.cs
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -2,30 +2,86 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ShopList.Gui.ViewModels
 {
-    public class ShopListViewModel
+    public class ShopListViewModel : INotifyPropertyChanged
     {
+        private string _nombreDelArticulo = string.Empty;
+        private int _cantidadAComprar = 1;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         public ObservableCollection<Item> Items { get; }
 
-        public ShopListViewModel()
+        public string NombreDelArticulo
+        {
+            get => _nombreDelArticulo;
+             set
+             {
+                if (value != _nombreDelArticulo)
+                {
+                    _nombreDelArticulo = value;
+                    OnPropertyChanged(nameof(NombreDelArticulo));
+                }
+             }
+        }
+        public int CantidadAComprar
+        {
+            get => _cantidadAComprar;
+            set
             {
+                if (value != _cantidadAComprar)
+                {
+                    _cantidadAComprar = value;
+                    OnPropertyChanged (nameof(CantidadAComprar));
+                }
+            }
+        }
+
+        public ICommand AgregarShopListItemCommand { get; private set; }
+
+        public ShopListViewModel()
+        {
             Items = new ObservableCollection<Item>();
             CargarDatos();
+            AgregarShopListItemCommand = new Command(AgregarShopListItem);
+        }
+
+        public void AgregarShopListItem()
+        {
+            if (string.IsNullOrEmpty(NombreDelArticulo)
+                ||CantidadAComprar <= 0)
+            {
+                return;
             }
+            Random generador = new Random();
+            var item = new Item
+            {
+                Id = generador.Next(),
+                Nombre = NombreDelArticulo,
+                Cantidad = CantidadAComprar,
+                Comprado = false,
+            };
+            Items.Add(item);
+            NombreDelArticulo= string.Empty;
+            CantidadAComprar= 1;
+        }
 
+       
         private void CargarDatos()
-
         {
             Items.Add(new Item()
             {
                 Id = 1,
                 Nombre = "Leche",
-                Cantidad = 1,
+                Cantidad = 2,
+                Comprado = false,
             });
 
 
@@ -34,14 +90,21 @@ namespace ShopList.Gui.ViewModels
                 Id = 2,
                 Nombre = "Pan de caja",
                 Cantidad = 1,
+                Comprado = true,
             });
             Items.Add(new Item()
             {
                 Id = 3,
                 Nombre = "Jamon",
                 Cantidad = 500,
+                Comprado = false,
             });
         }
-   }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 
 }
